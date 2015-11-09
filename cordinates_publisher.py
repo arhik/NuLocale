97add334bef5fdbd29c0dc6c974e8f1b44350381
arhik <kkatipally@umassd.edu>
2015-11-09 07:15:05 -0500

    make it executable

--- cordinates_publisher.py
+++ cordinates_publisher.py
@@ -24,49 +24,49 @@ clock = pygame.time.Clock()
 
 
 
-		
-		
-pub = rospy.Publisher('sensor_stream', Vector3, queue_size=10)
-rospy.init_node('sensor_input_publisher', anonymous=True)
-light_toggle = 0
-light_color = (30,30,30)
-mouse_mode = None
 
 #auto_mouse()
-while not rospy.is_shutdown():
-	try:
-		if mouse_mode==None:
+if(__name__=="__main__"):
+	pub = rospy.Publisher('sensor_stream', Vector3, queue_size=10)
+	rospy.init_node('sensor_input_publisher', anonymous=True)
+	light_toggle = 0
+	light_color = (30,30,30)
+	mouse_mode = None
+	while not rospy.is_shutdown():
 		
-			mouse_mode = "I am running already"
-		clock.tick(10)
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
+		try:
+			if mouse_mode==None:
+			
+				mouse_mode = "I am running already"
+			clock.tick(10)
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					sys.exit()
 
-			if event.type == pygame.K_DOWN:
-				sys.exit()
+				if event.type == pygame.K_DOWN:
+					sys.exit()
 
-			elif event.type ==pygame.MOUSEBUTTONDOWN:
-				light_toggle = 1 if light_toggle==0 else 0
-				light_color = (240,220,250) if light_toggle else (130,130,130)
-		screen.fill((25,25,25))
-		mousePosition = pygame.mouse.get_pos()
-		x, y = mousePosition
-		screen.blit(rendered_dot, (x,y))
-		pygame.draw.rect(screen, light_color, (400,400,100,100),0)
-		pygame.display.update()
-		rand_x = random.random()
-		rand_y = random.random()
-		val_x = x + 100*rand_x
-		val_y = y + 100*rand_y
-		val_z = float(light_toggle)
-		val = Vector3(val_x, val_y, val_z)
-		
-		rospy.loginfo("Sent %s at %s" %(val,rospy.get_time()))
-		pub.publish(val)
+				elif event.type ==pygame.MOUSEBUTTONDOWN:
+					light_toggle = 1 if light_toggle==0 else 0
+					light_color = (240,220,250) if light_toggle else (130,130,130)
+			screen.fill((25,25,25))
+			mousePosition = pygame.mouse.get_pos()
+			x, y = mousePosition
+			screen.blit(rendered_dot, (x,y))
+			pygame.draw.rect(screen, light_color, (400,400,100,100),0)
+			pygame.display.update()
+			rand_x = random.random()
+			rand_y = random.random()
+			val_x = x + 100*rand_x
+			val_y = y + 100*rand_y
+			val_z = float(light_toggle)
+			val = Vector3(val_x, val_y, val_z)
+			
+			rospy.loginfo("Sent %s at %s" %(val,rospy.get_time()))
+			pub.publish(val)
 
-	except KeyboardInterrupt:
-		sys.exit()
+		except KeyboardInterrupt:
+			sys.exit()
 
 
 	
